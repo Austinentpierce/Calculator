f862da4c841cf9a52d2d381a30462227331b4ef5
Austin Pierce <Austinentpierce@gmail.com>
2021-08-05 14:03:29 -0400

    calc working project practice

--- Program.cs
+++ Program.cs
@@ -2,11 +2,18 @@
 
 namespace Calculator
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter another number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(num1 + num2);
+
+            Console.ReadLine();
+        }
     }
-  }
 }
